6335252aaf698f14f0b052ff86cb8369300cb5cd
Tim Croteau <timcroteau1208@gmail.com>
2012-10-10 17:38:10 -0400

    HTML and Styles done. ready for validation

--- build/index.php
+++ build/index.php
@@ -15,19 +15,27 @@
         // if the form has been submitted, process it - otherwise, just display the form normally
         if(isset($_POST['send'])){
             
-            // pull the name out of the submitted form
-            $name = strip_tags($_POST['name']);
+            // pull submitted info out of the form
+            $port_title = strip_tags($_POST['port_title']);            
             
-            // pull the email out of the submitted form
+            $port_address = strip_tags($_POST['port_address']);
+            $name = strip_tags($_POST['name']);
             $emailFrom = strip_tags($_POST['email']);
+            $password = strip_tags($_POST['password']);
+            $cc_number = strip_tags($_POST['cc_number']);
+            $sec_code = strip_tags($_POST['sec_code']);
+            $month = strip_tags($_POST['month']);
+            $year = strip_tags($_POST['year']);
             
             // who you're sending the email to (probably change this)
             $emailTo = "tim.croteau@freshtilledsoil.com";
-            $subject = "Submission";
+            $subject = "Whoo! Signup Form Submission";
             
             // inset information into the body of the email
             $body = "Name: ".$name."\n";
             $body .= "Email: ".$emailFrom."\n";
+            $body .= "Portfolio Title: ".$port_title."\n";
+            $body .= "Portfolio Address: ".$port_address.".sample.com\n";
             
             // set the email headers
             $headers = "From: ".$emailFrom."\n";
@@ -55,53 +63,55 @@
                     <legend id="one">First, name your portfolio</legend>
                     <div>
                         <label for="port_title">Portfolio title</label>
-                        <input type="text" id="port_title" name="port_title" minlength="2"/>
+                        <input type="text" id="port_title" name="port_title" minlength="2" autofocus="autofocus" required/>
 
                         <label for="port_address">Portfolio address</label>
-                        <input type="text" id="port_address" name="port_address" minlength="2"/>
+                        <input type="text" id="port_address" name="port_address" minlength="2" required/>
                     </div>
                 </fieldset>
                 <fieldset form="whoo_signup">
                     <legend id="two">Now, enter your account details</legend>
                     <div>
                         <label for="name">Name</label>
-                        <input type="text" id="name" name="name" minlength="2"/>
+                        <input type="text" id="name" name="name" minlength="2" required/>
 
                         <label for="port_title">Email</label>
-                        <input type="text" id="email" name="email" minlength="2"/>
+                        <input type="email" id="email" name="email" minlength="2" required/>
                         <label class="disclosure">NOTE: We'll never share your email, promise.</label>
 
                         <label for="port_title">Password</label>
-                        <input type="text" id="password" name="password" minlength="2"/>
+                        <input type="text" id="password" name="password" minlength="2" required/>
                     </div>
                 </fieldset>
                 <fieldset form="whoo_signup">
                     <legend id="three">Finally, enter your payment information</legend>
                     <div>
                         <label for="cc_number">Card number</label>
-                        <input type="text" id="cc_number" name="cc_number" minlength="2"/>
+                        <input type="text" id="cc_number" name="cc_number" minlength="2" required/>
                         <div id="card_image"></div>
                         
                         <label for="sec_code">Security code</label>
-                        <input type="text" id="sec_code" name="sec_code" minlength="2"/>
+                        <input type="text" id="sec_code" name="sec_code" minlength="2" required/>
                         <div id="sec_image"></div>
 
                         <label for="month">Expiration date</label>
-                        <select id="month" name="month">
-                            <option>January</option>
-                            <option>February</option>
-                            <option>March</option>
-                            <option>April</option>
-                            <option>May</option>
-                            <option>June</option>
-                            <option>July</option>
-                            <option>August</option>
-                            <option>September</option>
-                            <option>October</option>
-                            <option>November</option>
-                            <option>December</option>
+                        <select id="month" name="month" required>
+                            <option value="">---</option>
+                            <option value="Jan">January</option>
+                            <option value="Feb">February</option>
+                            <option value="Mar">March</option>
+                            <option value="Apr">April</option>
+                            <option value="May">May</option>
+                            <option value="Jun">June</option>
+                            <option value="Jul">July</option>
+                            <option value="Aug">August</option>
+                            <option Value="Sep">September</option>
+                            <option value="Oct">October</option>
+                            <option value="Nov">November</option>
+                            <option value="Dec">December</option>
                         </select>
-                        <select id="year" name="year">
+                        <select id="year" name="year" required>
+                            <option value="">---</option>
                             <?php 
                                 for ($i = date("Y");$i <= (date("Y") + 5);$i++) {  
                                     echo '<option value="'.$i.'">'.$i.'</option>';
